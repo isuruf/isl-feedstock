328bed91baa95eae0461566b1b0be1bc2ae34a76
Isuru Fernando <isuruf@gmail.com>
2017-11-04 11:36:12 -0500

    Release mode and install files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ copy %RECIPE_DIR%\isl_config.h.cmake .
 copy %RECIPE_DIR%\isl_srcdir.c.cmake .
 mkdir build
 cd build
-cmake -G"NMake Makefiles" ..
+cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 cmake --build .
 cmake --build . --target install
 ctest -V
