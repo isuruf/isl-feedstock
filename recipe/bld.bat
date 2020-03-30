fea3ded6098aa444587fd0d592467276e721b099
Isuru Fernando <isuruf@gmail.com>
2020-03-30 16:03:49 -0500

    Print config.log

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ set CC=clang
 set ac_cv_have_decl__BitScanForward=yes
 
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
-bash -lc "cd $SRC_DIR &&  ./configure --with-int=imath CFLAGS='-O3 -Dstrdup=_strdup' --prefix=$PREFIX/Library"
+bash -lc "cd $SRC_DIR &&  ./configure --with-int=imath CFLAGS='-O3 -Dstrdup=_strdup' --prefix=$PREFIX/Library || (cat config.log; false)"
 bash -lc "cd $SRC_DIR && make -j$CPU_COUNT"
 bash -lc "cd $SRC_DIR && make check"
 bash -lc "cd $SRC_DIR && make install-strip"
