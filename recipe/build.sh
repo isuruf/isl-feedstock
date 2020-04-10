b35cad382ebe52b3a1457e1d8f8abd2afaaad72d
Isuru Fernando <isuruf@gmail.com>
2020-04-09 23:36:10 -0500

    set ac_cv_have_decl__BitScanForward=yes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 if [[ "$target_platform" == "amd64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
+  export ac_cv_have_decl__BitScanForward=yes
   ./configure --prefix=$PREFIX --with-int=imath || (cat config.log && false)
   patch_libtool
 else
