58632f5ea971df49a7575b6438122594231144c0
Isuru Fernando <isuruf@gmail.com>
2020-04-09 23:51:38 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-if [[ "$target_platform" == "amd64" ]]; then
+if [[ "$target_platform" == "amd64" || "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
   ./configure --prefix=$PREFIX --with-int=imath || (cat config.log && false)
