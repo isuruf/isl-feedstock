f1d39df216b641aa5217e4487c0c39023616fc98
Isuru Fernando <isuruf@gmail.com>
2020-04-09 23:54:14 -0500

    no undefined

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 if [[ "$target_platform" == "amd64" || "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
+  export LDFLAGS="$LDFLAGS -no-undefined"
   ./configure --prefix=$PREFIX --with-int=imath || (cat config.log && false)
   patch_libtool
 else
