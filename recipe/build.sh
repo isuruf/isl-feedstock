50c3576e7bdff5613f3f8e0f7054a7d222eaed26
Isuru Fernando <isuruf@gmail.com>
2020-04-09 21:11:10 -0500

    Use autotools_clang_conda in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
-if [[ "`uname`" == "Linux" ]]
-then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+if [[ "$target_platform" == "win-64" ]]; then
+  ./configure --with-int=imath CFLAGS='$CFLAGS -O3 -Dstrdup=_strdup' --prefix=$PREFIX
+  patch_libtool
+else
+  ./configure --prefix="$PREFIX" --with-gmp-prefix="$PREFIX"
 fi
-
-./configure --prefix="$PREFIX" --with-gmp-prefix="$PREFIX"
 make -j$CPU_COUNT
 make check
 make install-strip
