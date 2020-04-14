e307e7a575b69009b8cd98246714e0c9a3b6c0b8
Isuru Fernando <isuruf@gmail.com>
2020-04-14 08:59:39 -0500

    disable shared in windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@ if [[ "$target_platform" == "amd64" || "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
   autoreconf -iv
-  ./configure --prefix=$PREFIX --with-int=imath || (cat config.log && false)
+  ./configure --prefix=$PREFIX --with-int=imath --disable-shared || (cat config.log && false)
   patch_libtool
 else
   ./configure --prefix="$PREFIX" --with-gmp-prefix="$PREFIX"
