e89e519c215e1d6e143b6e31473ab691cbb482c0
Isuru Fernando <isuruf@gmail.com>
2020-04-14 08:29:04 -0500

    Remove setting LDFLAGS in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
 if [[ "$target_platform" == "amd64" || "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
-  export LDFLAGS="$LDFLAGS -no-undefined"
   ./configure --prefix=$PREFIX --with-int=imath || (cat config.log && false)
   patch_libtool
 else
