cad655b2e9b9e0a5ee5322ad50482ebd12af429b
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-09 13:55:31 +0200

    CFEP-18: Remove static libraries from main build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ if [[ "$target_platform" == "amd64" || "$target_platform" == "win-64" ]]; then
   ./configure --prefix=$PREFIX --with-int=imath --disable-shared || (cat config.log && false)
   patch_libtool
 else
-  ./configure --prefix="$PREFIX" --with-gmp-prefix="$PREFIX"
+  ./configure --prefix="$PREFIX" --with-gmp-prefix="$PREFIX" --disable-static
 fi
 make -j$CPU_COUNT
 make check
