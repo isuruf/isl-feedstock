32d96d5a4425b5ed04c4664de083e67d852a1a12
Isuru Fernando <isuruf@gmail.com>
2020-09-03 02:46:05 -0500

    osx-arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,19 @@
-if [[ "$target_platform" == "amd64" || "$target_platform" == "win-64" ]]; then
+#!/bin/bash
+
+if [[ "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
   autoreconf -iv
   ./configure --prefix=$PREFIX --with-int=imath --disable-shared || (cat config.log && false)
   patch_libtool
 else
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
   ./configure --prefix="$PREFIX" --with-gmp-prefix="$PREFIX" --disable-static
 fi
+
 make -j$CPU_COUNT
-make check
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
+  make check
+fi
 make install-strip
