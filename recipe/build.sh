2242687404f91e1438b22dcfaffb0af4417593c2
Isuru Fernando <isuruf@gmail.com>
2020-10-04 00:23:32 -0500

    Use mpir on windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ if [[ "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
   autoreconf -iv
-  ./configure --prefix=$PREFIX --with-int=imath --disable-shared || (cat config.log && false)
+  ./configure --prefix=$PREFIX --with-gmp-prefix="${PREFIX}" --disable-static || (cat config.log && false)
   patch_libtool
 else
   # Get an updated config.sub and config.guess
